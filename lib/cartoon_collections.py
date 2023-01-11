24411f7b68cf4e18e2d5b19966538bd113538baf
Ricardo Guerra <RikkuG491@gmail.com>
2023-01-11 14:50:28 -0500

    Completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index in range(len(dwarves)):
+        print(f"{index + 1}. {dwarves[index]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f"{planeteer_calls[index].capitalize()}!" for index in range(len(planeteer_calls))]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for index in range(len(calls)):
+        if(len(calls[index]) > 4):
+            return True
 
-def find_the_cheese():
-    pass
+    return False
+
+def find_the_cheese(words):
+    for index in range(len(words)):
+        if words[index] == 'cheddar' or words[index] == 'gouda' or words[index] == 'camembert':
+            return words[index]
+
+    return None
