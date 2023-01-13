2420f8789d3af498497ae80c146b3c1b3f969165
Liz Burton <liz.burton147@gmail.com>
2023-01-13 17:42:47 -0500

    Revert "Completed lab"
    This reverts commit 24411f7b68cf4e18e2d5b19966538bd113538baf.

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,20 +1,11 @@
-def roll_call_dwarves(dwarves):
-    for index in range(len(dwarves)):
-        print(f"{index + 1}. {dwarves[index]}")
+def roll_call_dwarves():
+    pass
 
-def summon_captain_planet(planeteer_calls):
-    return [f"{planeteer_calls[index].capitalize()}!" for index in range(len(planeteer_calls))]
+def summon_captain_planet():
+    pass
 
-def long_planeteer_calls(calls):
-    for index in range(len(calls)):
-        if(len(calls[index]) > 4):
-            return True
+def long_planeteer_calls():
+    pass
 
-    return False
-
-def find_the_cheese(words):
-    for index in range(len(words)):
-        if words[index] == 'cheddar' or words[index] == 'gouda' or words[index] == 'camembert':
-            return words[index]
-
-    return None
+def find_the_cheese():
+    pass
